016bee7902b68eb7afac18934cc578d226a914bd
Raleigh Fatoki <fatoki09@yahoo.com>
2014-02-17 00:27:49 -0600

    added patient scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 IllinoisCompassion::Application.routes.draw do
+  resources :patients
+
+  get "info/index"
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'info#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
