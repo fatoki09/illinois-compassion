7aadfb436ba7c230058cbf8c3a2b116615c9cc5b
Raleigh Fatoki <fatoki09@yahoo.com>
2014-02-21 04:14:09 -0600

    adjusting mailer for heroku

--- app/mailers/patient_mailer.rb
+++ app/mailers/patient_mailer.rb
@@ -1,7 +1,7 @@
 class PatientMailer < ActionMailer::Base
-  default to: "fatoki09@yahoo.com, joshua.chestang@gmail.com"
+  default to: "fatoki09@yahoo.com, joshua.chestang@gmail.com",
   				from: "fatoki09@yahoo.com"
-  
+
 
   def welcome_email(patient)
   	@patient = patient
